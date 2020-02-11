634dcfa7a4b10170cedb925a96a24d9fd8f1193d
Herculano <wagherculano@hotmail.com>
2020-02-10 23:45:04 -0300

    Exercicios Resolvidos: Funções

--- 05_Exercicios_Funcoes/14-QuadradoMagico.py
+++ 05_Exercicios_Funcoes/14-QuadradoMagico.py
@@ -11,52 +11,58 @@ Dica: produza todas as combinações possíveis e verifique a soma quando comple
 Usar um vetor de 1 a 9 parece ser mais simples que usar uma matriz 3x3.
 """
 
-vetor = list(range(1, 10))
+from itertools import permutations
 
 
-def quadrado_magico(lista_de_numeros):
-    # tamanho do vetor
-    tam = len(lista_de_numeros)
-    # nova lista com o tamanho do da variavel vetor
-    lista = [0] * tam
+def test_magic_square(array):
+    """
+    Verifica se as combinações são iguais a 15
+    """
+    if len(array) != 9:
+        return False
 
-    # como o vetor vai de 1 a 9, então a metade vale 5
-    # então 5 vai para o meio da variavel lista 5 e será excluida da variavel vetor.
-    lista[tam // 2] = lista_de_numeros.pop(lista_de_numeros.index(lista_de_numeros[tam // 2]))
+    # Lines
+    for i in range(0, 7, 3):
+        if sum(array[i:i + 3]) != 15:
+            return False
 
-    # listas onde serão guardados os numeros pares e impares
-    pares, impares = [], []
-    for x, y in enumerate(vetor):
-        if y % 2 == 0:
-            pares.append(y)
-        else:
-            impares.append(y)
+    # Columns
+    for i in range(3):
+        if sum(array[i::3]) != 15:
+            return False
 
-    def cria_quadrado():
-        # variaveis que serão usadas para os indices
-        par = 0
-        imp = 1
+    # Diagonal 1
+    for i in range(3):
+        if sum(array[::4]) != 15:
+            return False
 
-        # união dos conjuntos dos numeros, sendo o conjunto impar em ordem decrescente
-        for p, i in zip(pares, reversed(impares)):
-            # como o 5 ja está na posição 4, preciso jogar o 6 para a outra posição
-            if par == 4:
-                par = 6
-            lista[par] = p
-            lista[imp] = i
-            # aqui apenas incremento para que os valores possam ir para as posições corretas
-            par += 2
-            imp += 2
-
-        def imprime_quadrado_magico():
-            for k, v in enumerate(lista, start=1):
-                print(v, end=' ')
-                if k % 3 == 0:
-                    print()
-
-        return imprime_quadrado_magico()
-
-    return cria_quadrado()
+    # Diagonal 2
+    for i in range(3):
+        if sum(array[2:8:2]) != 15:
+            return False
+    return True
 
 
-quadrado_magico(vetor)
+def show_magic_square(combinations):
+    """
+    printa todas as combinações
+    """
+    cont = 1
+    for items in combinations:
+        print(f' {cont}a COMBINACAO '.center(21, '#'))
+        for i in range(0, 7, 3):
+            print(*items[i:i + 3])
+        print()
+        cont += 1
+
+
+# Fazer permutação entre a quantidade de números do range. 9 neste caso (1,2,3,4,5,6,7,8,9) (1,3,5,6,2,7,4,9,8)...
+# Se desejar fazer combinação de 2 numeros por exemplo, usar permutations(range(10), 2)
+vector = permutations(range(1, 10))
+
+magic_square = []
+for array in vector:
+    if test_magic_square(array):
+        magic_square.append(array)
+
+show_magic_square(magic_square)
