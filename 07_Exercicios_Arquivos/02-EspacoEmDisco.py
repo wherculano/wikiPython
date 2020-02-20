9ff6b5d141ff53b6af5ae6998a773b284e48bafc
Herculano <wagherculano@hotmail.com>
2020-02-20 13:02:52 -0300

    Exercicios Resolvidos: Arquivos

---
+++ 07_Exercicios_Arquivos/02-EspacoEmDisco.py
@@ -0,0 +1,54 @@
+"""
+A ACME Inc., uma empresa de 500 funcionários, está tendo problemas de espaço em disco no seu servidor de arquivos.
+Para tentar resolver este problema, o Administrador de Rede precisa saber qual o espaço ocupado pelos usuários,
+e identificar os usuários com maior espaço ocupado.
+Através de um programa, baixado da Internet, ele conseguiu gerar o seguinte arquivo, chamado "usuarios.txt":
+alexandre       456123789
+anderson        1245698456
+antonio         123456456
+carlos          91257581
+cesar           987458
+rosemary        789456125
+Neste arquivo, o nome do usuário possui 15 caracteres.
+A partir deste arquivo, você deve criar um programa que gere um relatório, chamado "relatório.txt", no seguinte formato:
+
+ACME Inc.               Uso do espaço em disco pelos usuários
+------------------------------------------------------------------------
+Nr.  Usuário        Espaço utilizado     % do uso
+
+1    alexandre       434,99 MB             16,85%
+2    anderson       1187,99 MB             46,02%
+3    antonio         117,73 MB              4,56%
+4    carlos           87,03 MB              3,37%
+5    cesar             0,94 MB              0,04%
+6    rosemary        752,88 MB             29,16%
+
+Espaço total ocupado: 2581,57 MB
+Espaço médio ocupado: 430,26 MB
+O arquivo de entrada deve ser lido uma única vez, e os dados armazenados em memória, caso sejam necessários,
+de forma a agilizar a execução do programa. A conversão da espaço ocupado em disco,
+de bytes para megabytes deverá ser feita através de uma função separada, que será chamada pelo programa principal.
+O cálculo do percentual de uso também deverá ser feito através de uma função, que será chamada pelo programa principal.
+"""
+soma = 0
+mb = 1024 ** 2
+usuarios = []
+espacos = []
+with open('02-Usuarios.txt', 'r') as users:
+    for i, v in enumerate(users.readlines()):
+        soma += int(v) if i % 2 != 0 else 0
+        if i % 2 == 0:
+            usuarios.append(v.replace('\n', ''))
+        else:
+            espacos.append(int(v))
+users.close()
+
+with open('02-Relatorios.txt', 'a+') as relat:
+    cont = 1
+    relat.write("""
+ACME Inc.     Uso do espaco em disco pelos usuarios
+------------------------------------------------------------------------
+Nr.  Usuario        Espaco utilizado     % do uso\n""")
+    for u, e in zip(usuarios, espacos):
+        relat.write(f'{cont:<3} {u:<15} {e / mb:<10.2f}{"MB":<2} {(e / mb) / (soma / mb) * 100:>15.2f}%\n')
+        cont += 1
